bace3255d84f79ec8ae5d74d9ff86efbb955af8e
Mauricio Cuello <mdavid_cuello@javeriana.edu.co>
2024-01-16 17:53:09 +0100

    Conditionals for editor options

--- vim_config.sh
+++ vim_config.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
-if [ -f /etc/vim/vimrc ]; then
-	vim_file=/etc/vim/vimrc
-else
-	touch ~/.vim
-	vim_file=~/.vim
-fi
-
-
-if [ -d ~/.config/nvim/ ]; then
-	if [ -f ~/.config/nvim/init.vim ]; then
-		vim_file=~/.config/nvim/init.vim
+# If the inline editor is vim the command is the following
+# vim_config.sh vim
+# the other option is neovim
+if [[ $1 = "vim" ]]; then 
+	if [ -d ~/.config/nvim/ ]; then
+		if [ -f ~/.config/nvim/init.vim ]; then
+			vim_file=~/.config/nvim/init.vim
+		else
+			touch ~/.config/nvim/init.vim
+			vim_file=~/config/nvim/init.vim
 	else
-		touch ~/.config/nvim/init.vim
-		vim_file=~/config/nvim/init.vim
+		mkdir ~/config/nvim/
+		touch ~/config/nvim/init.vim
+	fi
+elif [[ $1 = "neovim" ]]; then
+	if [ -f /etc/vim/vimrc ]; then
+		vim_file=/etc/vim/vimrc
+	else
+		touch ~/.vim
+		vim_file=~/.vim
+	fi
 else
-	mkdir ~/config/nvim/
-	touch ~/config/nvim/init.vim
+	echo "There is no option for that editor"
 fi
+
 
 echo "let skip_defaults_vim=1
 set wrap
