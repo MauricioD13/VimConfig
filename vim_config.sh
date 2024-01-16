437d211047acc724f429133a040114e43ea1931f
Mauricio Cuello <mdavid_cuello@javeriana.edu.co>
2024-01-16 17:32:42 +0100

    File for configure neovim

---
+++ vim_config.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+if [ -f /etc/vim/vimrc ]; then
+	vim_file=/etc/vim/vimrc
+else
+	touch ~/.vim
+	vim_file=~/.vim
+fi
+
+
+if [ -d ~/.config/nvim/ ]; then
+	if [ -f ~/.config/nvim/init.vim ]; then
+		vim_file=~/.config/nvim/init.vim
+	else
+		touch ~/.config/nvim/init.vim
+		vim_file=~/config/nvim/init.vim
+else
+	mkdir ~/config/nvim/
+	touch ~/config/nvim/init.vim
+fi
+
+echo "let skip_defaults_vim=1
+set wrap
+syntax enable
+set wildmenu
+set showcmd
+set incsearch
+set clipboard=unnamed
+set autoindent
+set nostartofline
+set number
+set numberwidth=1
+set encoding=utf8
+set showmatch
+set relativenumber
+set mouse=a
+set ignorecase
+set title
+set background=dark" >> $vim_file
+# Mauro edit (Mark for bash search)
+
+
+
